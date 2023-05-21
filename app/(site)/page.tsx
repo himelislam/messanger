23e5f18a9fba2372403be790177480cb3f1330f5
Himel Islam <himel7100@gmail.com>
2023-05-21 20:49:56 +0600

    first commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+    return (
+      <main >
+       <h1 className='text-3xl font-bold text-sky-500 underline'>Hello Worid from Inside site</h1> 
+      </main>
+    )
+  }
+  
